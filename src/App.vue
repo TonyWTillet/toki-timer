e5be2c6de5ae4eb46ace7595699e661263b6889f
tonytillet <tony.tillet@gmail.com>
2025-03-26 01:21:59 +0100

    Init repository, add vue + ts + shadcn/vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+</script>
+
+<template>
+  <main class="bg-zinc-950 dark:bg-white">
+    <div class="flex flex-col items-center justify-center h-screen">
+      <h1 class="text-4xl font-bold text-white">Hello World</h1>
+    </div>
+  </main>
+</template>
