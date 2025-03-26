89aece1602aa969992e24006dd4860285cdcbc80
tonytillet <tony.tillet@gmail.com>
2025-03-26 01:31:59 +0100

    Refactor App.vue to replace static content with a structured Card component for improved UI presentation.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 </script>
 
 <template>
   <main class="bg-zinc-950 dark:bg-white">
     <div class="flex flex-col items-center justify-center h-screen">
-      <h1 class="text-4xl font-bold text-white">Hello World</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle><h1>TokiTimer, a timer for studying</h1></CardTitle>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          Card Content
+        </CardContent>
+        <CardFooter>
+          Card Footer
+        </CardFooter>
+      </Card>
     </div>
   </main>
 </template>
